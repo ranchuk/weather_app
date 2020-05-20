bc21630777fead8c895ebba0f8b1e829584bcc9e
Ranchuk <rannnz1@gmail.com>
2020-05-20 14:39:01 +0300

    lazy loading routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import './App.css';
 import Menu from './components/menuBar/menuBar.js'
-import Home from './components/home/home.js'
-import Favorites from './components/favorites/favorites.js'
+// import Home from './components/home/home.js'
+// import Favorites from './components/favorites/favorites.js'
 import { Provider } from "react-redux";
 import store from "./store";
 import GeoLocation from './geoLocationAPI'
@@ -10,6 +10,10 @@ import { BrowserRouter, Route } from "react-router-dom";
 import {setFavorites} from './actions/favoritesAction'
 import { SnackbarProvider } from 'notistack';
 import Notifier from './components/Notifier/Notifier';
+
+const Home = lazy(() => import('./components/home/home.js'));
+const Favorites = lazy(() => import('./components/favorites/favorites.js'));
+
 GeoLocation()
 
 if (localStorage.favorites) {
@@ -21,19 +25,21 @@ function App () {
     <Provider store={store}>
       <SnackbarProvider maxSnack={4}>
       <Notifier />
-        <BrowserRouter>
-            <Menu/>
-            <Route
-              path="/"
-              component={Home}
-              exact
-            />
-            <Route
-              path="/favorites"
-              component={Favorites}
-              exact
-            />
-        </BrowserRouter>
+      <Suspense fallback={<div>Loading...</div>}>
+          <BrowserRouter>
+              <Menu/>
+              <Route
+                path="/"
+                component={Home}
+                exact
+              />
+              <Route
+                path="/favorites"
+                component={Favorites}
+                exact
+              />
+          </BrowserRouter>
+        </Suspense>
       </SnackbarProvider>
     </Provider>
     );
